fb26af8c8c4ae98935a0ad44bac7cf10dfaa2460
xbony2 <xbony22@gmail.com>
2014-12-16 08:03:23 -0500

    Made the screwdriver work more or less (damageScrewdriver method is not complete, no negative checks or anything), moved IScrewdriver to new package

--- src/main/java/cubewrench202/powerred/api/items/IScrewdriver.java
+++ src/main/java/cubewrench202/powerred/api/items/IScrewdriver.java
@@ -1,8 +1,8 @@
-package cubewrench202.powerred.api.energy;
+package cubewrench202.powerred.api.items;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 public interface IScrewdriver {
-public void damageScrewdriver(World world, EntityPlayer player);
+	public void damageScrewdriver(World world, EntityPlayer player);
 }
--- src/main/java/cubewrench202/powerred/item/ItemScrewdriver.java
+++ src/main/java/cubewrench202/powerred/item/ItemScrewdriver.java
@@ -1,5 +1,6 @@
 package cubewrench202.powerred.item;
 
+import cubewrench202.powerred.api.items.IScrewdriver;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -9,26 +10,28 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class ItemScrewdriver extends Item{
+public class ItemScrewdriver extends Item implements IScrewdriver{
 	
 	public ItemScrewdriver() {
+	
 	this.setMaxDamage(145);
 	this.setMaxStackSize(1);
 	this.setNoRepair();
 	this.setFull3D();
-}
+	}
 
-@Override
-public boolean onItemUseFirst(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
-{
-	Block b = world.getBlock( x, y, z );
-	if ( b != null && !player.isSneaking() )
-	{
-		ForgeDirection mySide = ForgeDirection.getOrientation( side );
-		
+	@Override
+	public boolean onItemUseFirst(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ){
+		Block b = world.getBlock(x, y, z);
+		if (b != null && !player.isSneaking()){
+			ForgeDirection mySide = ForgeDirection.getOrientation( side );
 		}
-	
-	return true;
-}
+		return true;
+	}
 
+	@Override
+	public void damageScrewdriver(World world, EntityPlayer player) {
+		ItemStack driver = player.getItemInUse();
+		driver.setItemDamage(driver.getItemDamage() - 1);
+	}
 }
